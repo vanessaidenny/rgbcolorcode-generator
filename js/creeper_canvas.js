2d8306a059912e55a2aa2db5d3c8f10d2253fc07
Vanessa Isabela Denny <vanessaidenny@hotmail.com>
2020-07-20 09:21:15 -0300

    Add background img and style and increased canvas width

--- js/creeper_canvas.js
+++ js/creeper_canvas.js
@@ -5,27 +5,27 @@ pincel.strokeStyle = 'black';
 
 //head
 pincel.fillStyle = 'darkgreen';
-pincel.fillRect(40, 15, 115, 100);
-pincel.strokeRect(40, 15, 115, 100);
+pincel.fillRect(70, 15, 115, 100);
+pincel.strokeRect(70, 15, 115, 100);
 //body
-pincel.fillRect(65, 115, 65, 50);
-pincel.strokeRect(65, 115, 65, 50);
+pincel.fillRect(95, 115, 65, 50);
+pincel.strokeRect(95, 115, 65, 50);
 //feet
-pincel.fillRect(50, 145, 50, 50);
-pincel.strokeRect(50, 145, 50, 50);
-pincel.fillRect(100, 145, 45, 50);
-pincel.strokeRect(100, 145, 45, 50);
+pincel.fillRect(80, 145, 50, 50);
+pincel.strokeRect(80, 145, 50, 50);
+pincel.fillRect(130, 145, 45, 50);
+pincel.strokeRect(130, 145, 45, 50);
 //eyes
 pincel.fillStyle = 'black';
-pincel.fillRect(55, 35, 30, 30);
-pincel.strokeRect(55, 35, 30, 30);
-pincel.fillRect(110, 35, 30, 30);
-pincel.strokeRect(110, 35, 30, 30);
+pincel.fillRect(85, 35, 30, 30);
+pincel.strokeRect(85, 35, 30, 30);
+pincel.fillRect(140, 35, 30, 30);
+pincel.strokeRect(140, 35, 30, 30);
 //nose
-pincel.fillRect(85, 65, 25, 30);
-pincel.strokeRect(85, 65, 25, 30);
+pincel.fillRect(115, 65, 25, 30);
+pincel.strokeRect(115, 65, 25, 30);
 //mouth
-pincel.fillRect(110, 80, 10, 35);
-pincel.strokeRect(110, 80, 10, 35);
-pincel.fillRect(75, 80, 10, 35);
-pincel.strokeRect(75, 80, 10, 35);
+pincel.fillRect(140, 80, 10, 35);
+pincel.strokeRect(140, 80, 10, 35);
+pincel.fillRect(105, 80, 10, 35);
+pincel.strokeRect(105, 80, 10, 35);
